b6ae54e9d7f3bebde654596ae68297ae6a9695fa
linuxbt <peterchen9736@proton.me>
2024-07-23 14:50:52 +0800

    Update k.sh  更新kjj_menu

--- k.sh
+++ k.sh
@@ -7877,6 +7877,7 @@ execute_command() {
 }
 
 # 函数：交互式菜单
+# 函数：交互式菜单
 kjj_menu() {
     local menu="$(cat $kjj_file | jq .menu)"
     local path=()
@@ -7894,8 +7895,14 @@ kjj_menu() {
             exit 0
         elif [[ "$choice" == "0" ]]; then
             if [[ ${#path[@]} -gt 0 ]]; then
-                unset path[${#path[@]}-1]
-                menu=$(cat $kjj_file | jq ".menu${path:+.$(IFS=.; echo "${path[*]}")}.submenu")
+                unset path[${#path[@]}-1]  # 移除当前菜单
+                if [[ ${#path[@]} -gt 0 ]]; then
+                    # 返回上一级菜单
+                    menu=$(cat $kjj_file | jq ".menu${path:+.$(IFS=.; echo "${path[*]}")}.submenu")
+                else
+                    # 如果已经返回到最上层，获取根菜单
+                    menu=$(cat $kjj_file | jq ".menu")
+                fi
             fi
         else
             local selected=$(echo "$menu" | jq ".\"$choice\"")
