59ab1e9ae4ad4bae04167299cf8afb3e7f20b573
linuxbt <peterchen9736@proton.me>
2024-07-27 17:07:29 +0800

    Update install_3x-ui.sh

--- sh/install_3x-ui.sh
+++ sh/install_3x-ui.sh
@@ -62,19 +62,6 @@ show_info() {
 
 }
 # 安装3x-ui
-# install_3x-ui() {
-#     check_docker
-#     docker run -itd \
-# 	-e XRAY_VMESS_AEAD_FORCED=false \
-# 	-v $PWD/db/:/etc/x-ui/ \
-# 	-v $PWD/cert/:/root/cert/ \
-# 	--network=host \
-# 	--restart=unless-stopped \
-# 	--name 3x-ui \
-# 	ghcr.io/mhsanaei/3x-ui:latest
-#     sleep 8
-# }
-
 install_3x-ui() {
     check_docker
     docker run -itd \
@@ -85,18 +72,8 @@ install_3x-ui() {
 	--restart=unless-stopped \
 	--name 3x-ui \
 	ghcr.io/mhsanaei/3x-ui:latest
-
-    # 等待3x-ui容器启动完成
-    echo -n "正在启动3x-ui容器..."
-    while [ -z "$(docker ps -q -f name=3x-ui)" ]; do
-        sleep 1
-        echo -n "."
-    done
-    echo " 完成"
-
-    # 显示信息
-    show_info
 }
+
 
 
 # 定义3x-ui面板函数
@@ -124,7 +101,9 @@ x-ui_menu() {
               clear
               send_stats "安装 3x-ui "
               install_3x-ui
+	      sleep 8
               show_info
+
               ;;
 
           2)
