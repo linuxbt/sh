a5a33b94f14e374ba7d81059b24c5388b346ce27
linuxbt <peterchen9736@proton.me>
2024-08-10 11:51:57 +0800

    Update install_candy_client.sh

--- sh/install_candy_client.sh
+++ sh/install_candy_client.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+# 检查是否安装Docker
+if ! command -v docker &> /dev/null; then
+    echo "Docker未安装，正在安装..."
+    bash <(curl -sSL https://get.docker.com) || bash <(curl -sSL https://raw.githubusercontent.com/linuxbt/sh/main/sh/install_docker.sh)
+    sudo systemctl enable docker
+    sudo systemctl start docker
+else
+    echo "Docker已安装"
+fi
+
+mkdir -p /var/lib/candy  && docker run --detach --restart=always --privileged=true --net=host --volume /var/lib/candy:/var/lib/candy docker.io/lanthora/candy:latest
+systemctl enable --now candy
