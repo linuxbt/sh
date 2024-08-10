b09e2c906b3a0594a406a3bb313ca2a0b43df9c7
linuxbt <peterchen9736@proton.me>
2024-08-10 11:58:30 +0800

    Update install_candy_client.sh

--- sh/install_candy_client.sh
+++ sh/install_candy_client.sh
@@ -11,3 +11,10 @@ fi
 
 mkdir -p /var/lib/candy  && docker run --detach --restart=always --privileged=true --net=host --volume /var/lib/candy:/var/lib/candy docker.io/lanthora/candy:latest
 systemctl enable --now candy
+
+candy_ip=ip a
+# 输出用户信息
+echo "安装完成"
+echo "----------------------以下是您的candy网络相关配置信息--------------------------------"
+echo "组网IP: ip a 自行查询下"
+echo "--------------组网配置后，可以使用ping命令或其他nc命令测试网络延迟-------------"
