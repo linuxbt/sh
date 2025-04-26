3b5249397a1857d9e047b18c4e0d4ee21fcdf664
linuxbt <peterchen9736@proton.me>
2025-04-27 00:03:13 +0800

    Update bip39_encry.sh

--- sh/bip39_encry.sh
+++ sh/bip39_encry.sh
@@ -2305,6 +2305,7 @@ generate_and_encrypt() {
     # 使用 -pass pass:"$password" 直接传递密码
     # IMPORTANT: openssl enc output includes Salted__ header and base64.
     # Using "-a" for base64 encoding.
+    hash -r # Force shell to re-find openssl
     encrypted_string=$(printf "%s" "$mnemonic" | openssl enc $OPENSSL_OPTS -pass pass:"$password_input")
 
     local openssl_exit_code=$?
@@ -2368,6 +2369,7 @@ decrypt_and_display() {
     # 使用 2> /dev/null 隐藏 openssl 的错误信息 (如 bad decrypt)
     # openssl base64 decode (-d -a) is part of the decryption process when using -a flag
     # Need to pipe the input string to openssl
+    hash -r # Force shell to re-find openssl
     decrypted_mnemonic=$(printf "%s" "$encrypted_string_input" | openssl enc -d $OPENSSL_OPTS -pass pass:"$password_input" 2> /dev/null)
 
     local openssl_exit_code=$?
